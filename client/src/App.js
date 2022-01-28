4ffcb3cfc8d89633f83441514b574e1634820c40
Adam <a.6942@yahoo.com>
2022-01-28 13:21:55 -0500

    changed file structure

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+    <div>
+     <Header></Header>
+    </div>
+    <div>
+      <Footer></Footer>
+    </div>
+    </>
+  );
+}
+
+export default App;
