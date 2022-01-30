234cece3e81cebaea0ace1815a7b3be471de78ef
Adam <a.6942@yahoo.com>
2022-01-30 11:52:24 -0500

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,53 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
-const App = () => {
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Nav from "./components/Nav";
+import { StoreProvider } from "./utils/GlobalState";
+import OrderHistory from "./pages/OrderHistory";
+import Success from "./pages/Success";
+
+
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
+})
+
+function App() {
   return (
-    <>
-    <div>
-     <Header></Header>
-    </div>
-    <div>
-      <Footer></Footer>
-    </div>
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+        <StoreProvider>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/orderHistory" component={OrderHistory} />
+            <Route exact path="/products/:id" component={Detail} />
+            <Route exact path="/success" component={Success} />
+            <Route component={NoMatch} />
+          </Switch>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
+
   );
 }
 
