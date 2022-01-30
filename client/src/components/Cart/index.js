d4feceed977e50a7cd1f9b36ea48ca14638c90a2
Adam <a.6942@yahoo.com>
2022-01-30 18:28:29 -0500

    working on getting products to load

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import './style.css';
+//import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -44,7 +44,7 @@ function Nav() {
       <h1>
         <Link to="/">
           <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          Retro Gaming Store
         </Link>
       </h1>
 
